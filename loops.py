189b280a2fb92701ff74b5327bf74d4f50367001
😜 🤫 <nataliakal4enko@gmail.com>
2024-02-06 20:01:03 +0200

    Add loops

---
+++ loops.py
@@ -0,0 +1,12 @@
+for i in [0, 1, 2, 3, 4, 5]:
+    print(i)
+
+for i in range(6):
+    print(i)    
+names = ["Harry", "Ron", "Hermione"] 
+for name in names:
+    print(name)
+
+word = "Hello"
+for letter in word:
+    print(letter)
